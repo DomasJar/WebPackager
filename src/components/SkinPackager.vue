a6771d110bf553e8ecfdaad387ea7539f4abd5df
DomasJar <domasjava@hotmail.com>
2023-02-11 05:05:05 +0200

    Store assets init

--- src/components/SkinPackager.vue
+++ src/components/SkinPackager.vue
@@ -5,13 +5,11 @@ import FileSaver from 'file-saver';
 import draggable from 'vuedraggable';
 import { v4 as uuidv4 } from 'uuid';
 
-
-
-const pack_name = ref("Skinpack")
-let isPackaging = ref(false)
-let drag = ref(false)
-let skins = reactive<Array<{ name: string, type: "custom" | "customSlim", fileName: string, url: string, file: File }>>([])
-
+interface marketImage {
+  fileName: string,
+  url: string,
+  file: File
+}
 
 interface skinsJson {
   serialize_name: string,
@@ -34,6 +32,13 @@ interface manifestJson {
   ],
   format_version: number
 }
+
+
+const pack_name = ref("Skinpack")
+let isPackaging = ref(false)
+let drag = ref(false)
+let skins = reactive<Array<{ name: string, type: "custom" | "customSlim", fileName: string, url: string, file: File }>>([])
+let keyArt = <marketImage>{}
 
 async function packageSkins() {
   try {
@@ -78,7 +83,7 @@ function generateZip() {
         await zip.file(`Content/skin_pack/${skinFileName}`, skin.file)
         langFileContent += `\nskin.${packNameProccesed}.${skinFileName}=${skin.name}`
         skinsJsonContent.skins.push({
-          localization_name:  skinFileName,
+          localization_name: skinFileName,
           geometry: `geometry.humanoid.${skinFileName}`,
           texture: `${skinFileName}.png`,
           type: "paid"
@@ -95,6 +100,43 @@ function generateZip() {
       reject()
     }
   })
+}
+
+async function sortImages(event: Event) {
+  const target = (<HTMLInputElement>event.target)
+  let entries: File[] = []
+  if (target && target.files) {
+    try {
+      entries = Object.values(target.files);
+      for (const val of entries) {
+        let imgUrl = URL.createObjectURL(val)
+        if (val.type == "image/png") {
+          skins.push({
+            fileName: val.name,
+            name: val.name.replace(/\.[^/.]+$/, ""),
+            url: imgUrl,
+            file: val,
+            type: await getSkinType(imgUrl)
+          })
+        } else if (val.type == "image/jpeg") {
+          let size = await getImgSize(imgUrl)
+          if (size.width == 800 && size.height == 450) {
+            keyArt.file = val,
+              keyArt.fileName = val.name,
+              keyArt.url = imgUrl
+          } else if (size.width == 1920 && size.height == 1080) {
+            console.log(val);
+
+          }
+        }
+      }
+      console.log(keyArt);
+    } catch (error) {
+      console.error(error);
+
+    }
+
+  }
 }
 
 async function displayskins(event: Event) {
@@ -121,7 +163,7 @@ function getSkinType(imageUrl: string): Promise<"custom" | "customSlim"> {
 
     img.src = imageUrl;
     img.onload = function () {
-      
+
       let canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
       canvas.width = img.width;
       canvas.height = img.height;
@@ -129,27 +171,39 @@ function getSkinType(imageUrl: string): Promise<"custom" | "customSlim"> {
         "2d"
       ) as CanvasRenderingContext2D;
       ctx.drawImage(img, 0, 0);
-      
+
       let resolutionMultiplier = img.height / 64;
-      console.log(ctx.getImageData(50 * resolutionMultiplier, 16 * resolutionMultiplier, 1 * resolutionMultiplier, 1 * resolutionMultiplier));
       let handData = ctx.getImageData(50 * resolutionMultiplier, 16 * resolutionMultiplier, 2 * resolutionMultiplier, 4 * resolutionMultiplier);
       let armData = ctx.getImageData(54 * resolutionMultiplier, 20 * resolutionMultiplier, 2 * resolutionMultiplier, 12 * resolutionMultiplier);
 
       for (let i = 0; i < armData.data.length; i += 4) {
-        console.log(armData.data[i+3]);
-        if(armData.data[i+3] > 0){
+        if (armData.data[i + 3] > 0) {
           resolve("custom")
         }
       }
 
       for (let i = 0; i < handData.data.length; i += 4) {
-        if(handData.data[i+3] > 0){
+        if (handData.data[i + 3] > 0) {
           resolve("custom")
         }
       }
-      
+
       resolve("customSlim")
     };
+  })
+}
+
+function getImgSize(src: string): Promise<{ width: number, height: number }> {
+  return new Promise(async (resolve, reject) => {
+    const newImg = new Image();
+
+    newImg.onload = function () {
+      const height = newImg.height;
+      const width = newImg.width;
+      resolve({ width, height })
+    };
+
+    newImg.src = src; // this must be done AFTER setting onload
   })
 }
 
@@ -166,18 +220,19 @@ function getSkinType(imageUrl: string): Promise<"custom" | "customSlim"> {
     <v-row>
       <v-col>
         <v-text-field variant="outlined" label="Skinpack name" v-model="pack_name"></v-text-field>
-        <v-file-input variant="outlined" label="File input" multiple @change="displayskins"></v-file-input>        
+        <v-file-input variant="outlined" label="Images" multiple accept="image/png, image/jpeg"
+          @change="sortImages"></v-file-input>
       </v-col>
     </v-row><v-row>
       <v-col cols="12">
-        <draggable class="d-flex align-center justify-center row flex-wrap" :list="skins" group="people"
-          @start="drag = true" @end="drag = false" item-key="element.url">
+        <draggable class="d-flex align-center justify-center row flex-wrap" ghost-class="moving-card" :list="skins"
+          group="people" :animation="200" @start="drag = true" @end="drag = false" item-key="element.url">
           <template #item="{ element }">
-            <v-card class="flex-grow-0 ma-5">
+            <v-card rounded="lg" class="flex-grow-0 ma-5">
               <v-card-title>
                 {{ skins.indexOf(element) + "_" + element.type }}
               </v-card-title>
-              <v-text-field  label="Character name" v-model="element.name"></v-text-field>
+              <v-text-field label="Character name" v-model="element.name"></v-text-field>
               <v-img width="300" :src="element.url"></v-img>
             </v-card>
           </template>
@@ -194,6 +249,10 @@ function getSkinType(imageUrl: string): Promise<"custom" | "customSlim"> {
   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.moving-card {
+  opacity: 0.5;
+  background-color: gray;
+  /* @apply opacity-50 bg-gray-100 border border-blue-500; */
+}
 </style>
