fd13c71e451d8c17d4e6117b82fcf9ac9b4844b6
DomasJar <domasjava@hotmail.com>
2023-02-11 03:07:58 +0200

    deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/WebPackager/",
   plugins: [
     vue(),
     vuetify({ autoImport: true, styles: { configFile: 'src/styles/settings.scss' } }), // Enabled by default
